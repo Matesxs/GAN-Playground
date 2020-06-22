efaebd52ead97aeed7473acc9f58744aa72926d6
Martin Dousa <martindousa186@gmail.com>
2020-06-22 20:49:04 +0200

    more preps

--- modules/models/upscaling_generator_models_spreadsheet.py
+++ modules/models/upscaling_generator_models_spreadsheet.py
@@ -3,7 +3,7 @@ from keras.layers import Layer, BatchNormalization, Conv2D, PReLU, Add
 
 from modules.models.custom_layers import deconv_layer, res_block
 
-def mod_srgan_3upscl(inp:Layer, start_image_shape:tuple, kernel_initializer:Initializer=RandomNormal(stddev=0.02)):
+def mod_srgan_base(inp:Layer, start_image_shape:tuple, num_of_upscales:int, kernel_initializer:Initializer=RandomNormal(stddev=0.02)):
 
 	m = Conv2D(filters=64, kernel_size=9, strides=1, padding="same", kernel_initializer=kernel_initializer, input_shape=start_image_shape)(inp)
 	m = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(m)
@@ -17,7 +17,7 @@ def mod_srgan_3upscl(inp:Layer, start_image_shape:tuple, kernel_initializer:Init
 	m = BatchNormalization(momentum=0.5)(m)
 	m = Add([skip, m])
 
-	for _ in range(3):
+	for _ in range(num_of_upscales):
 		m = deconv_layer(m, 256, kernel_size=3, strides=2, leaky=True, kernel_initializer=kernel_initializer)
 
 	m = Conv2D(filters=start_image_shape[2], kernel_size=9, strides=1, padding="same", activation="tanh", kernel_initializer=kernel_initializer)(m)
--- modules/srgan.py
+++ modules/srgan.py
@@ -1,17 +1,22 @@
 import colorama
+import os
 from typing import Union
 import tensorflow as tf
 import keras.backend as K
 from keras.optimizers import Optimizer, Adam
 from keras.models import Model
 from keras.applications.vgg19 import VGG19
+from keras.initializers import RandomNormal
+import cv2 as cv
+
+from modules.custom_tensorboard import TensorBoardCustom
 
 tf.get_logger().setLevel('ERROR')
 colorama.init()
 
 # Calculate start image size based on final image size and number of upscales
 def count_upscaling_start_size(target_image_shape: tuple, num_of_upscales: int):
-	upsc = (target_image_shape[0] // (2 ** num_of_upscales), target_image_shape[1] // (2 ** num_of_upscales))
+	upsc = (target_image_shape[0] // (2 ** num_of_upscales), target_image_shape[1] // (2 ** num_of_upscales), target_image_shape[2])
 	if upsc[0] < 1 or upsc[1] < 1: raise Exception(f"Invalid upscale start size! ({upsc})")
 	return upsc
 
@@ -36,11 +41,57 @@ class SRGAN:
 	GRADIENT_CHECK_INTERVAL = 10  # Interval of checking norm gradient value of combined model
 	CHECKPOINT_SAVE_INTERVAL = 1  # Interval of saving checkpoint
 
-	def __init__(self, dataset_path:str,
+	def __init__(self, dataset_path:str, num_of_upscales:int,
 	             gen_mod_name: str, disc_mod_name: str,
 	             training_progress_save_path:str=None,
 	             generator_optimizer: Optimizer = Adam(0.0002, 0.5), discriminator_optimizer: Optimizer = Adam(0.0002, 0.5),
 	             batch_size: int = 32, buffered_batches:int=20,
 	             generator_weights: Union[str, None, int] = None, discriminator_weights: Union[str, None, int] = None,
 	             start_episode: int = 0, load_from_checkpoint: bool = False):
-		pass
+
+		self.disc_mod_name = disc_mod_name
+		self.gen_mod_name = gen_mod_name
+		self.num_of_upscales = num_of_upscales
+
+		self.batch_size = batch_size
+
+		if start_episode < 0: start_episode = 0
+		self.epoch_counter = start_episode
+
+		# Create array of input image paths
+		self.train_data = [os.path.join(dataset_path, file) for file in os.listdir(dataset_path)]
+		self.data_length = len(self.train_data)
+
+		# Load one image to get shape of it
+		self.target_image_shape = cv.imread(self.train_data[0]).shape
+
+		# Check image size validity
+		if self.target_image_shape[0] < 4 or self.target_image_shape[1] < 4: raise Exception("Images too small, min size (4, 4)")
+
+		# Starting image size calculate
+		self.start_image_shape = count_upscaling_start_size(self.target_image_shape, self.num_of_upscales)
+
+		# Check validity of whole datasets
+		self.validate_dataset()
+
+		# Initialize training data folder and logging
+		self.training_progress_save_path = training_progress_save_path
+		self.tensorboard = None
+		if self.training_progress_save_path:
+			self.training_progress_save_path = os.path.join(self.training_progress_save_path, f"{self.gen_mod_name}__{self.disc_mod_name}__{self.start_image_shape}_to_{self.target_image_shape}")
+			self.tensorboard = TensorBoardCustom(log_dir=os.path.join(self.training_progress_save_path, "logs"))
+
+		# Create array of input image paths
+		self.train_data = [os.path.join(dataset_path, file) for file in os.listdir(dataset_path)]
+		self.data_length = len(self.train_data)
+
+		# Define static vars
+		self.kernel_initializer = RandomNormal(stddev=0.02)
+
+	# Check if datasets have consistent shapes
+	def validate_dataset(self):
+		for im_path in self.train_data:
+			im_shape = cv.imread(im_path).shape
+			if im_shape != self.target_image_shape:
+				raise Exception("Inconsistent datasets")
+		print("Dataset valid")
