a703fedff055dca135bffa45157e9c8dbc1dea89
Martin Dousa <martindousa186@gmail.com>
2020-06-27 17:07:11 +0200

    minor changes

--- modules/models/upscaling_generator_models_spreadsheet.py
+++ modules/models/upscaling_generator_models_spreadsheet.py
@@ -61,3 +61,24 @@ def mod_srgan_ext(inp:Layer, start_image_shape:tuple, num_of_upscales:int, kerne
 
   m = Conv2D(filters=start_image_shape[2], kernel_size=9, strides=1, padding="same", activation="tanh", kernel_initializer=kernel_initializer, use_bias=False)(m)
   return m
+
+def mod_srgan_ext_sub(inp:Layer, start_image_shape:tuple, num_of_upscales:int, kernel_initializer:Initializer=RandomNormal(stddev=0.02)):
+  m = Conv2D(filters=64, kernel_size=9, strides=1, padding="same", kernel_initializer=kernel_initializer, input_shape=start_image_shape, activation=None)(inp)
+  m = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(m)
+
+  skip = m
+
+  for _ in range(16):
+    m = res_block(m, 64, kernel_size=3, strides=1, batch_norm=0.5, use_bias=False, kernel_initializer=kernel_initializer)
+
+  m = Conv2D(filters=64, kernel_size=3, strides=1, padding="same", kernel_initializer=kernel_initializer, use_bias=False, activation=None)(m)
+  m = BatchNormalization(momentum=0.5, axis=-1)(m)
+  m = Add()(inputs=[skip, m])
+
+  for _ in range(num_of_upscales):
+    m = deconv_layer(m, 256, kernel_size=3, strides=2, leaky=True, batch_norm=None, use_subpixel_conv2d=True, upsample_first=False, use_bias=False, kernel_initializer=kernel_initializer)
+
+  m = identity_layer(m, 256, kernel_size=3, batch_norm=0.5, dropout=None, use_bias=False, kernel_initializer=kernel_initializer)
+
+  m = Conv2D(filters=start_image_shape[2], kernel_size=9, strides=1, padding="same", activation="tanh", kernel_initializer=kernel_initializer, use_bias=False)(m)
+  return m
