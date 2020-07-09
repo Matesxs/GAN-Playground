f2f2ddf8cd9f6abb6b7d8d9d2f12d0b1a0016e65
Martin Dousa <martindousa186@gmail.com>
2020-07-09 03:15:26 +0200

    upgraded descriptions in settings

--- settings.py
+++ settings.py
@@ -1,17 +1,19 @@
 ################################
 ### Settings of classic GANs ###
 ################################
+# Data source settings
 DATASET_PATH = "datasets/dogs_normalized__64x64__train"
 TESTING_DATASET_PATH = "datasets/dogs_normalized__64x64__test"
 
+# Training settings
+START_EPISODE = 0
 NUM_OF_TRAINING_EPISODES = 1_000_000
 
-LATENT_DIM = 128
 BATCH_SIZE = 16
 BUFFERED_BATCHES = 50
 
-START_EPISODE = 0
-
+# Model settings
+LATENT_DIM = 128
 GEN_MODEL = "mod_ext2_4upscl"
 GEN_WEIGHTS = None
 DISC_MODEL = "mod_ext_5layers"
@@ -21,25 +23,27 @@ DICS_WEIGHTS = None
 ##########################################
 ### Settings of SR GANs and SR Resnets ###
 ##########################################
+# Data source settings
 DATASET_SR_PATH = "datasets/all_normalized__256x256"
 TESTING_DATASET_SR_PATH = None
 # If none will be provided then script will select some random one
 CUSTOM_HR_TEST_IMAGE = r"F:\Projekty\Python\GANTest\datasets\all_normalized__256x256\60165.png"
 
-# Discriminator will be trained more on images where its more behind
-AUTOBALANCE_TRAINING_OF_SRGAN = True
-
+# Training settings
+START_EPISODE_SR = 0
 COMBINED_TRAINING_EPISODES_SRGAN = 100_000
 GENERATOR_TRAIN_EPISODES_OF_SRGAN = 1_500_000
 # With generator too (Discriminator need to catch up with generator before it will adding details to image)
 DISCRIMINATOR_TRAIN_EPISODES_OF_SRGAN = 500_000
 FINETUNE_TRAIN_EPISODES_OF_SRGAN = 200_000
 
+# Discriminator will be trained more on images where its more behind
+AUTOBALANCE_TRAINING_OF_SRGAN = True
+
 BATCH_SIZE_SR = 4
 BUFFERED_BATCHES_SR = 50
 
-START_EPISODE_SR = 0
-
+# Model settings
 NUM_OF_UPSCALES = 2
 GEN_SR_MODEL = "mod_srgan_base_sub"
 GEN_SR_WEIGHTS = None
@@ -50,16 +54,22 @@ DICS_SR_WEIGHTS = None
 ########################
 ### General settings ###
 ########################
+# Number of batches used for testing step
 NUM_OF_TEST_BATCHES = 10
 
+# Check if you want to load last autocheckpoint (If weights were provided thne checkpoint will be overriden by them)
 LOAD_FROM_CHECKPOINTS = True
-# Leave this false only when you are sure your dataset is consistent
+# Leave this false only when you are sure your dataset is consistent (Check whole dataset if all images have same dimensions before training)
 CHECK_DATASET = False
 
+# Num of episodes after whitch weights will be saved (Its not the same as checkpoint!)
 WEIGHTS_SAVE_INTERVAL = 10_000
+# Num of episodes after whitch progress image/s will be created to "track" progress of training
 PROGRESS_IMAGE_SAVE_INTERVAL = 2_500
 # Save progress images to folder too (if false then they will be saved only to tensorboard)
 SAVE_RAW_IMAGES = True
+# Duration of one frame if gif is created from progress images after training
 GIF_FRAME_DURATION = 100
 
+# Num of worker used to preload data for training/testing
 NUM_OF_LOADING_WORKERS = 8
