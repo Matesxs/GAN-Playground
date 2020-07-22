4f63b7eddb50dc0fc65871b71ec605b9a83fd8f0
Martin Dousa <martindousa186@gmail.com>
2020-07-22 16:29:26 +0200

    refactoring

--- scrape_lorem_picsum.py
+++ scrape_lorem_picsum.py
@@ -11,23 +11,6 @@ SAVE_PATH = "datasets/random_images"
 PATH_TO_CHROME_DRIVER = r"lib/chromedriver.exe"
 RESOLUTION = 1024
 
-driver = webdriver.Chrome(executable_path=PATH_TO_CHROME_DRIVER)
-
-image_urls = []
-for _ in tqdm(range(IMGS_TO_DOWNLOAD)):
-  try:
-    driver.get(f"https://picsum.photos/{RESOLUTION}")
-    el = driver.find_element_by_xpath("/html/body/img")
-    if el.get_attribute("src") not in image_urls:
-      image_urls.append(el.get_attribute("src"))
-    time.sleep(0.01)
-  except KeyboardInterrupt:
-    break
-
-print(f"Found {len(image_urls)} unique images")
-time.sleep(1)
-driver.close()
-
 def download_image(img_url):
   img_stream = requests.get(img_url, stream=True)
   img_stream.raw.decode_content = True
@@ -43,5 +26,23 @@ def download_image(img_url):
   del img_stream
   local_file.close()
 
-with Pool(processes=cpu_count()) as p:
-  p.map(download_image, image_urls)
+if __name__ == '__main__':
+  driver = webdriver.Chrome(executable_path=PATH_TO_CHROME_DRIVER)
+
+  image_urls = []
+  for _ in tqdm(range(IMGS_TO_DOWNLOAD)):
+    try:
+      driver.get(f"https://picsum.photos/{RESOLUTION}")
+      el = driver.find_element_by_xpath("/html/body/img")
+      if el.get_attribute("src") not in image_urls:
+        image_urls.append(el.get_attribute("src"))
+      time.sleep(0.01)
+    except KeyboardInterrupt:
+      break
+
+  print(f"Found {len(image_urls)} unique images")
+  time.sleep(1)
+  driver.close()
+
+  with Pool(processes=cpu_count()) as p:
+    p.map(download_image, image_urls)
