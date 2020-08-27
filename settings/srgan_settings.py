4f85a01518e4870b11f241a081fa684859c0fcab
Martin Dousa <martindousa186@gmail.com>
2020-08-27 23:21:09 +0200

    training srgan will be done only on faces (due to artifacts in non face images), upgraded preprocess dataset script

--- settings/srgan_settings.py
+++ settings/srgan_settings.py
@@ -1,8 +1,8 @@
 # Data source settings
-DATASET_PATH = "datasets/all_normalized__256x256__train"
-TESTING_DATASET_PATH = "datasets/all_normalized__256x256__test"
+DATASET_PATH = "datasets/faces_normalized__256x256__train"
+TESTING_DATASET_PATH = "datasets/faces_normalized__256x256__test"
 # If none will be provided then script will select some random one
-CUSTOM_HR_TEST_IMAGES = ["datasets/testing_image1.png", "datasets/testing_image2.png", "datasets/testing_image3.png", "datasets/testing_image4.png"]
+CUSTOM_HR_TEST_IMAGES = ["datasets/testing_image1.png", "datasets/testing_image2.png", "datasets/testing_image2.jpg"]
 
 # Training settings
 START_EPISODE = 0
