25f5f8be70a1a82465ebd98819b1a42466695cd9
Martin Dousa <martindousa186@gmail.com>
2020-08-28 14:53:07 +0200

    removed accumulation of gradients, its buggy, will try to recreate later

--- modules/keras_extensions/custom_lrscheduler.py
+++ modules/keras_extensions/custom_lrscheduler.py
@@ -1,7 +1,6 @@
 import keras.backend as K
 from typing import Union
 from keras import Model
-from keras.optimizers import Optimizer
 
 class LearningRateScheduler:
   """Learning rate scheduler."""
@@ -26,17 +25,5 @@ class LearningRateScheduler:
 
       new_lr = self.lr_plan[self.episode]
       K.set_value(object.optimizer.lr, new_lr)
-      self.lr = new_lr
-      return new_lr
-    elif isinstance(object, Optimizer):
-      if self.lr:
-        if float(K.get_value(object.lr)) != self.lr:
-          K.set_value(object.lr, self.lr)
-
-      if not self.lr_plan: return None
-      if not self.episode in self.lr_plan: return None
-
-      new_lr = self.lr_plan[self.episode]
-      K.set_value(object.lr, new_lr)
       self.lr = new_lr
       return new_lr
