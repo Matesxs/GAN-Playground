44730dd37f5b652a0d476ace9dfdb7195017abf0
Martin Dousa <martindousa186@gmail.com>
2020-08-28 12:41:10 +0200

    minor tweaks

--- modules/keras_extensions/custom_lrscheduler.py
+++ modules/keras_extensions/custom_lrscheduler.py
@@ -1,5 +1,7 @@
 import keras.backend as K
 from typing import Union
+from keras import Model
+from keras.optimizers import Optimizer
 
 class LearningRateScheduler:
   """Learning rate scheduler."""
@@ -10,18 +12,31 @@ class LearningRateScheduler:
     self.episode = 0
     self.lr = start_lr
 
-  def set_lr(self, model):
-    if not hasattr(model.optimizer, 'lr'):
-      raise ValueError('Optimizer must have a "lr" attribute.')
+  def set_lr(self, object):
+    if isinstance(object, Model):
+      if not hasattr(object.optimizer, 'lr'):
+        raise ValueError('Optimizer must have a "lr" attribute.')
 
-    if self.lr:
-      if float(K.get_value(model.optimizer.lr)) != self.lr:
-        K.set_value(model.optimizer.lr, self.lr)
+      if self.lr:
+        if float(K.get_value(object.optimizer.lr)) != self.lr:
+          K.set_value(object.optimizer.lr, self.lr)
 
-    if not self.lr_plan: return None
-    if not self.episode in self.lr_plan: return None
+      if not self.lr_plan: return None
+      if not self.episode in self.lr_plan: return None
 
-    new_lr = self.lr_plan[self.episode]
-    K.set_value(model.optimizer.lr, new_lr)
-    self.lr = new_lr
-    return new_lr
+      new_lr = self.lr_plan[self.episode]
+      K.set_value(object.optimizer.lr, new_lr)
+      self.lr = new_lr
+      return new_lr
+    elif isinstance(object, Optimizer):
+      if self.lr:
+        if float(K.get_value(object.lr)) != self.lr:
+          K.set_value(object.lr, self.lr)
+
+      if not self.lr_plan: return None
+      if not self.episode in self.lr_plan: return None
+
+      new_lr = self.lr_plan[self.episode]
+      K.set_value(object.lr, new_lr)
+      self.lr = new_lr
+      return new_lr
