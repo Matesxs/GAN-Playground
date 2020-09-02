f8bf1543e18c2e6e3bc37063d64a16049991e27c
Martin Dousa <martindousa186@gmail.com>
2020-09-02 02:18:43 +0200

    added testing script for parsing large images to small ones

--- parse_hr_image.py
+++ parse_hr_image.py
@@ -3,20 +3,31 @@ import cv2 as cv
 
 from modules.helpers import get_paths_of_files_from_path
 
-HR_IMAGES_FOLDER = r"C:\Users\marti\OneDrive\Pictures"
-OUTPUT_FOLDER = "test_folder"
+HR_IMAGES_FOLDER = r""
+OUTPUT_FOLDER = r""
 TARGET_SHAPE = (256, 256)
 
 assert os.path.exists(HR_IMAGES_FOLDER), "Input folder doesnt exist"
 if not os.path.exists(OUTPUT_FOLDER): os.makedirs(OUTPUT_FOLDER)
 
-hr_image_paths = get_paths_of_files_from_path(HR_IMAGES_FOLDER)
+hr_image_paths = get_paths_of_files_from_path(HR_IMAGES_FOLDER, only_files=True)
 assert len(hr_image_paths) > 0, "High resulution image folder is empty"
 
-for hr_image_path in hr_image_paths:
-  if not os.path.exists(hr_image_path): continue
+for idx, hr_image_path in enumerate(hr_image_paths):
+  if not os.path.exists(hr_image_path):
+    continue
 
   hr_image = cv.imread(hr_image_path)
-  if hr_image is None: continue
+  if hr_image is None:
+    continue
 
-  print(hr_image.shape[:-1])
+  iter_over_y = int(hr_image.shape[0] / TARGET_SHAPE[1])
+  iter_over_x = int(hr_image.shape[1] / TARGET_SHAPE[0])
+
+  if iter_over_x == 0 and iter_over_y == 0:
+    continue
+  
+  for y_idx in range(iter_over_y):
+    for x_idx in range(iter_over_x):
+      image_part = hr_image[y_idx * TARGET_SHAPE[1]:(y_idx + 1) * TARGET_SHAPE[1], x_idx * TARGET_SHAPE[0]:(x_idx + 1) * TARGET_SHAPE[0], :]
+      cv.imwrite(os.path.join(OUTPUT_FOLDER, f"{idx}_{(y_idx * TARGET_SHAPE[1]) + x_idx}.png"), image_part)
