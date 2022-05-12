ebd2c1614e6c3cacfc4739a8f54841fd3a170340
Martin Dousa <martindousa186@gmail.com>
2022-05-12 16:26:10 +0200

    added results for anime coloring using pix2pix

--- gans/Pix2Pix/settings.py
+++ gans/Pix2Pix/settings.py
@@ -4,41 +4,46 @@ from albumentations.pytorch import ToTensorV2
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-MODEL_NAME = "anime_pix2pix_model"
+MODEL_NAME = "maps_pix2pix_model"
 
-SWITCH_INPUT_IMAGE_POSITIONS = True
+SWITCH_INPUT_IMAGE_POSITIONS = False
 TESTING_SAMPLES = 8
-TRAINING_DATASET_PATH = "datasets/anime/train"
-TESTING_DATASET_PATH = "datasets/anime/val"
+TRAINING_DATASET_PATH = "datasets/maps/train"
+TESTING_DATASET_PATH = "datasets/maps/val"
 
 GEN_MODEL_WEIGHTS_TO_LOAD = None
 DISC_MODEL_WEIGHTS_TO_LOAD = None
 
 LR = 2e-4
-BATCH_SIZE = 16
+LR_DECAY = False
+LR_DECAY_COEF = 0.1
+LR_DECAY_EVERY = 50
+
+BATCH_SIZE = 1
 IMG_SIZE = 256
 IMG_CHAN = 3
 FEATURES_DISC = [64, 128, 256, 512] # [64 * 2, 128 * 2, 256 * 2, 512 * 2] [64, 128, 256, 512]
 FEATURES_GEN = [64, 128, 256, 512, 512, 512, 512] # [64 * 2, 128 * 2, 256 * 2, 512 * 2, 512 * 2, 512 * 2, 512 * 2] [64, 128, 256, 512, 512, 512, 512]
 GEN_UPSCALE_DROPOUT = [True, True, True, False, False, False, False]
 L1_LAMBDA = 100
-EPOCHS = 1000
+EPOCHS = 200
 
-both_transform = A.Compose(
-    [A.Resize(width=IMG_SIZE, height=IMG_SIZE), A.HorizontalFlip(p=0.5),], additional_targets={"image0": "image"},
+train_transform = A.Compose(
+  [
+    A.Resize(width=IMG_SIZE, height=IMG_SIZE),
+    A.HorizontalFlip(p=0.5),
+    #A.ColorJitter(p=0.1),
+    A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+    ToTensorV2(),
+  ],
+  additional_targets={"image0": "image"},
 )
 
-transform_only_input = A.Compose(
-    [
-        #A.ColorJitter(p=0.1),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
-        ToTensorV2(),
-    ]
-)
-
-transform_only_mask = A.Compose(
-    [
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
-        ToTensorV2(),
-    ]
+test_transform = A.Compose(
+  [
+    A.Resize(width=IMG_SIZE, height=IMG_SIZE),
+    A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+    ToTensorV2(),
+  ],
+  additional_targets={"image0": "image"},
 )
